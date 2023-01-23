d55296749f53e4f4a9892f69b2d0d54e29e9a937
yasvipatel52 <yasvi.patel@marutitech.com>
2023-01-23 11:18:41 +0530

    all apis implemented

--- js/script.js
+++ js/script.js
@@ -16,21 +16,79 @@ for(var i = 0; i < data.city.length; i++) {
 });
 
 
-// const select1=document.querySelector("countryDropDown");
-// console.log(select1.options[select1.selectedIndex].value);
-// const weather=fetch("https://goweather.herokuapp.com/weather/ny");
-// weather.then((response)=>{
-//     return response.json();
-// }).then((data)=>{
-//     console.log(data);
-// })
+// https://goweather.herokuapp.com/weather/${city}
+// api.openweathermap.org/data/2.5/weather?q=${city}&appid=add8a03a537edf8ad35013b54daa76d8
+
+ 
+function fetchingTemp(select){
+         console.log(select.value);
+         let city=select.value;
+         let weather=fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=9YUHX4EPRZT8SWMDXME7WWCQZ&contentType=json`);
+         weather.then((response) => {
+              return response.json();
+         }).then((data)=>{
+            console.log(data);
+            console.log(data.currentConditions.temp);
+            let temp1=document.getElementById('temp');
+            temp1.innerHTML=data.currentConditions.temp ;
+
+         })
+
+          
+        }
 
 
 
-const cards=fetch("https://raw.githubusercontent.com/Dipen-Dedania/static-data/main/make-your-trip-package.json");
-cards.then((response)=>{
-    return response.json();
-}).then((data)=>{
-    console.log(data);
-})
 
+
+
+ function fetchingCards(){
+    let url1 =  fetch('https://raw.githubusercontent.com/Dipen-Dedania/static-data/main/make-your-trip-package.json');
+    url1.then((response) => {
+        return response.json();
+      }).then((element) => {
+        console.log(element);
+        let cardContainerItem=document.getElementById('cardContainer');
+        for(let i=0;i<element.length;i++){
+            let divElement=document.createElement('div');
+            divElement.setAttribute('class', 'card');
+            let divElementPara1=document.createElement('p');
+            divElementPara1.setAttribute('class', 'cityName');
+            divElementPara1.textContent=element[i].cityName;
+            divElement.append(divElementPara1)
+            let divElementPara2=document.createElement('p');
+            divElementPara2.setAttribute('class', 'tourDate');
+            divElementPara2.textContent=element[i].tourDate;
+            divElement.append(divElementPara2)
+            let divElementPara3=document.createElement('p');
+            divElementPara3.setAttribute('class', 'category');
+            divElementPara3.textContent=element[i].category;
+            divElement.append(divElementPara3)
+            let divElementPara4=document.createElement('p');
+            divElementPara4.setAttribute('class', 'temp');
+            divElementPara4.textContent=element[i].temp;
+            divElement.append(divElementPara4)
+            cardContainerItem.append(divElement);
+            let hr=document.createElement('hr');
+            cardContainerItem.append(hr);
+
+        }
+        })
+    }
+
+
+fetchingCards();
+
+
+let date=document.getElementById('date');
+let day=document.getElementById('day');
+let mnth=document.getElementById('mnth');
+
+const d=new Date();
+date.innerHTML = d.getDate();
+
+const days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Suarday"];
+day.innerHTML=days[d.getDay()];
+
+const month=["January", "February", "March", "April", "May", "June","July","August","September","October","November","December"];
+mnth.innerHTML=`${month[d.getMonth()]},${d.getFullYear()}`;
