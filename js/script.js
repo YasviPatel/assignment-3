bfac06fb1dbadb092002ec4e2a763a455342da55
yasvipatel52 <yasvi.patel@marutitech.com>
2023-01-24 11:23:25 +0530

    almost done

--- js/script.js
+++ js/script.js
@@ -27,6 +27,8 @@ function fetchingTemp(select){
          }).then((data)=>{
             console.log(data);
             console.log(data.currentConditions.temp);
+            let cityName=document.getElementById('city');
+            cityName.innerHTML=city;
             let temp1=document.getElementById('temp');
             temp1.innerHTML=data.currentConditions.temp+"&#8451";
             let temp2=document.getElementById('icon');
@@ -65,18 +67,20 @@ function fetchingTemp(select){
         console.log(element);
         let cardContainerItem=document.getElementById('cardContainer');
         for(let i=0;i<element.length;i++){
+            let cardCon=document.createElement('div');
+            cardCon.setAttribute('class','cardCon');
             let divElement=document.createElement('div');
             divElement.setAttribute('class', 'card');
             let divElementPara1=document.createElement('p');
-            divElementPara1.setAttribute('class', 'cityName');
+            divElementPara1.setAttribute('class', 'cityName upper');
             divElementPara1.textContent=element[i].cityName;
             divElement.append(divElementPara1)
             let divElementPara2=document.createElement('p');
-            divElementPara2.setAttribute('class', 'tourDate');
+            divElementPara2.setAttribute('class', 'tourDate upper');
             divElementPara2.textContent=element[i].tourDate;
             divElement.append(divElementPara2)
             let divElementPara3=document.createElement('p');
-            divElementPara3.setAttribute('class', 'category');
+            divElementPara3.setAttribute('class', 'category upper');
             divElementPara3.textContent=element[i].category;
             divElement.append(divElementPara3)
             let divElementPara4=document.createElement('p');
@@ -86,22 +90,35 @@ function fetchingTemp(select){
             let divElementImg1=document.createElement('img');
             divElementImg1.setAttribute('class', 'cityImgage');
             divElementImg1.setAttribute('src',`${element[i].cityImg}`)
+            divElementImg1.setAttribute('height','200fr');
+            divElementImg1.setAttribute('width','100%');
             divElement.append(divElementImg1);
             let divElementPara5=document.createElement('p');
             divElementPara5.setAttribute('class', 'tourDate');
             divElementPara5.textContent=element[i].tourDate;
             divElement.append(divElementPara5);
+            let priceDiv=document.createElement('div');
+            priceDiv.setAttribute('class','priceDiv');
+            let totalPrice=document.createElement('div');
+            totalPrice.setAttribute('class','totalPrice');
+            let divElementPrice=document.createElement('p');
+            divElementPrice.textContent="Price";
+            totalPrice.append(divElementPrice);
             let divElementPara6=document.createElement('p');
             divElementPara6.setAttribute('class', 'price');
             divElementPara6.textContent=element[i].price;
-            divElement.append(divElementPara6);
-            let divElementPara7=document.createElement('p');
-            divElementPara7.setAttribute('class', 'isBookmark');
-            divElementPara7.textContent=element[i].isBookmark;
-            divElement.append(divElementPara7);
-            cardContainerItem.append(divElement);
-            let hr=document.createElement('hr');
-            cardContainerItem.append(hr);
+            totalPrice.append(divElementPara6);
+            let exploreButton=document.createElement('button');
+            exploreButton.textContent="Explore";
+            priceDiv.append(totalPrice);
+            priceDiv.append(exploreButton);
+            divElement.append(priceDiv);
+            // let divElementPara7=document.createElement('p');
+            // divElementPara7.setAttribute('class', 'isBookmark');
+            // divElementPara7.textContent=element[i].isBookmark;
+            // divElement.append(divElementPara7);
+            cardCon.append(divElement);
+            cardContainerItem.append(cardCon);
         }
     })
 }
